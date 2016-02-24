f97a6adfd439fecf25dfae4a0ac6a02adf11c56d
RachaelMack <rachael.mackeigan@gmail.com>
2016-02-24 16:18:44 -0400

    fix to browse page

--- assets/scripts/controllers/browseController.js
+++ assets/scripts/controllers/browseController.js
@@ -2,7 +2,15 @@
 
 angular.module('hgtApp').controller('browseController', function($scope, $http) {
  $scope.posts = [];
-  
+ $scope.searchPost = '';
+ $scope.loadPosts =  function (){
+ 	$http.get ('/api/post').then(function(result){
+ 		console.log ('finished');
+ 		$scope.posts = result.data;
+ 		// console.log (JSON.stringify (result.data));
+ 		// console.log ($scope.posts);
+ 	})
+ };
 
 
  });
